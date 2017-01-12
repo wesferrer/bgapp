6c508c437b31b74de9d2e34f34071af8be39920b
Wes Ferrer <wesferrer@gmail.copm>
2017-01-12 12:45:42 -0800

    sign up page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  private
+
+  helper_method :current_user
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def authorize
+    redirect_to login_path, alert: 'You must be logged in to use this feature' if current_user.nil?
+  end
 end
