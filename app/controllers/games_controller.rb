6f38801837d0ae1bf5ed6c0a5809ee32a8df6ef5
Wes Ferrer <wesferrer@gmail.copm>
2017-01-13 11:35:44 -0800

    fri 13

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -11,4 +11,24 @@ class GamesController < ApplicationController
   def show
     @game = Game.find(params[:id])
   end
+
+  def create
+    @game = Game.new(params.require(:game).permit(:name, :year, :designer, :publisher, :max_players))
+    if @game.save
+      redirect_to games_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+      @game = Game.find(params[:id])
+  end
+
+  def destroy
+    @game = Game.find(params[:id])
+    @Game.destroy if @Game.user == current_user
+    redirect_to game_path
+  end
+
 end
